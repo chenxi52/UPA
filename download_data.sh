be04143c8b16dbc50136526a43bd779f9a0b84f1
chenxi105 <1051180307@qq.com>
2024-03-17 23:21:29 +0800

    .

--- download_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mkdir data datasets/multi
-cd datasets/multi
-wget http://csr.bu.edu/ftp/visda/2019/multi-source/real.zip -O real.zip
-wget http://csr.bu.edu/ftp/visda/2019/multi-source/sketch.zip -O sketch.zip
-unzip real.zip
-unzip sketch.zip
-
